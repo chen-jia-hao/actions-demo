9bda49fa80dd70e6ff9da1c718b781e12f501ce1
chenjiahao <chen-jh@outlook.com>
2020-12-15 15:41:27 +0800

    add sckey

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const axios = require('axios')
 const qs = require('qs')
+const {format} = require('date-fns')
 
 const SCKEY = process.env.SCKEY
 
 async function init() {
   let data = {
     text: 'hi cjh',
-    desp: 'go go go'
+    desp: `date --> ${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`
   }
   let res = await axios.post(`https://sc.ftqq.com/${SCKEY}.send`, qs.stringify(data))
   console.log(res.data)
 }
 
 init().then(console.log)
-console.log(new Date().toLocaleDateString(), new Date().toLocaleTimeString())
-console.log(process.env.VAR1, process.env.VAR2, process.env.VAR3, SCKEY)
-console.log(process.env)
+console.log(format(new Date(), 'yyyy-MM-dd HH:mm:ss'))
