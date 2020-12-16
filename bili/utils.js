7793f4a3ed3b339ad11ba942c744fb2f4302d6ba
chen-jia-hao <chen-jia-hao@outlook.com>
2020-12-16 22:25:28 +0800

    print info

--- bili/utils.js
+++ bili/utils.js
@@ -5,6 +5,7 @@ const biliCookie = process.env.COOKIE
 
 // let cookie = 'LIVE_BUVID=AUTO4515451435298818; buvid3=7DF1AC5D-5C22-4FBF-B628-057713D9D3DB189471infoc; rpdid=wdospsopkxpw; fts=1546953353; INTVER=1; _uuid=E838AC91-E4EF-E340-E6B7-A8A1599F3E0582780infoc; CURRENT_FNVAL=80; blackside_state=1; sid=ag65t37s; DedeUserID=268457698; DedeUserID__ckMd5=b4a3d60cb0c342d6; SESSDATA=965b1cf4%2C1622376222%2C592eb*c1; bili_jct=f2a118fc95fb9c9a0320bb4b579ff9d5; CURRENT_QUALITY=80; PVID=1; bsource=search_baidu'
 // let biliCookie = 'DedeUserID=268457698;SESSDATA=965b1cf4%2C1622376222%2C592eb*c1;bili_jct=f2a118fc95fb9c9a0320bb4b579ff9d5'
+console.log(biliCookie);
 const biliCookieObj = cookie.parse(biliCookie)
 
 biliCookieObj.bili_jct || console.log('cookie 有误，请重新设置')
